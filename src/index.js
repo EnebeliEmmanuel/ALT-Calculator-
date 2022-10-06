8e49267a2373f8ac38b2f579366a6c2c79eb9b85
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-10-07 00:18:15 +0100

    [CHORE]-Calculator functionalities done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './css/styles.css';
+import './css/themes.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
